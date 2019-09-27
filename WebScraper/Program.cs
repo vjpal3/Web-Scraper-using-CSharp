ecf4565c57e79aa2354a3258a938c7cb1785d345
vjpal3 <vrishalipal@yahoo.com>
2019-09-27 10:59:46 -0400

    Login User with correct credentials

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Configuration;
+
 
 namespace WebScraper
 {
     class Program
     {
+        [Obsolete]
         static void Main(string[] args)
         {
             using (IWebDriver driver = new ChromeDriver())
             {
                 driver.Navigate().GoToUrl("https://login.yahoo.com/");
-                Console.WriteLine("Page Title: " + driver.Title);
+                
+
+                IWebElement username = driver.FindElement(By.Id("login-username"));
+                username.SendKeys("vrishalipal");
+                username.Submit();
+               
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                IWebElement password = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
+
+                string ypwd = ConfigurationManager.AppSettings["ypwd"];
+                password.SendKeys(ypwd);
+                password.SendKeys(Keys.Return);
+
+                Thread.Sleep(20000);
+
             }
         }
     }
